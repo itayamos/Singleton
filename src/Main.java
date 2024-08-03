09327c0758cf35d6cd90bc10d046550fe0ab2c60
Itay Amos <itayamos1144@gmail.com>
2024-08-03 19:31:15 +0300

    Initial commit of Parking Lot Manager project

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        // Initialize the ParkingLotManager with 5 available spaces
         ParkingLotManager manager = ParkingLotManager.getInstance(5);
 
+        // Create different types of vehicles using the VehicleFactory
         Vehicle car = VehicleFactory.createVehicle("car", "123-ABC");
         Vehicle motorcycle = VehicleFactory.createVehicle("motorcycle", "456-DEF");
         Vehicle truck = VehicleFactory.createVehicle("truck", "789-GHI");
 
+        // Park the vehicles in the parking lot
         manager.parkVehicle(car);
         manager.parkVehicle(motorcycle);
         manager.parkVehicle(truck);
 
+        // Vehicles leave the parking lot
         manager.leaveVehicle(car);
         manager.leaveVehicle(motorcycle);
 
+        // Create another car and park it in the parking lot
         Vehicle anotherCar = VehicleFactory.createVehicle("car", "101-JKL");
         manager.parkVehicle(anotherCar);
     }
